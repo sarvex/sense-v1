8097272ed3633b2349d3b10cbdbcdd41e4ad28a7
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2023-04-10 09:26:24 +0100

    chore: use quote.sellToken instead of getting underlying from adapter

--- pkg/core/src/Periphery.sol
+++ pkg/core/src/Periphery.sol
@@ -324,8 +324,7 @@ contract Periphery is Trust, IERC3156FlashBorrower {
         _transfer(quote.buyToken, receiver, amt);
 
         // refund any remaining underlying to receiver
-        ERC20 underlying = ERC20(Adapter(adapter).underlying());
-        _transfer(underlying, receiver, underlying.balanceOf(address(this)));
+        _transfer(quote.sellToken, receiver, quote.sellToken.balanceOf(address(this)));
     }
 
     /// @notice Adds liquidity providing any Token
@@ -419,8 +418,7 @@ contract Periphery is Trust, IERC3156FlashBorrower {
         _transfer(quote.buyToken, receiver, amt);
 
         // refund any remaining underlying to receiver
-        ERC20 underlying = ERC20(Adapter(adapter).underlying());
-        _transfer(underlying, receiver, underlying.balanceOf(address(this)));
+        _transfer(quote.sellToken, receiver, quote.sellToken.balanceOf(address(this)));
     }
 
     /* ========== UTILS ========== */
@@ -484,8 +482,7 @@ contract Periphery is Trust, IERC3156FlashBorrower {
         _transfer(quote.buyToken, receiver, amt);
 
         // refund any remaining underlying to receiver
-        ERC20 underlying = ERC20(Adapter(adapter).underlying());
-        _transfer(underlying, receiver, underlying.balanceOf(address(this)));
+        _transfer(quote.sellToken, receiver, quote.sellToken.balanceOf(address(this)));
     }
 
     /* ========== ADMIN ========== */
